c798c6983e1cbfe1d42d0c8b4aec8d49405d0303
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-04-20 11:20:51 -0300

    Create Pacient.java

---
+++ exampleClasses/Pacient.java
@@ -0,0 +1,16 @@
+package timoteoclass;
+
+public abstract class Pacient {
+
+    public abstract void healthy();
+
+    public abstract void gender();
+
+    public abstract void name();
+
+    public abstract void age();
+
+    public abstract void document();
+
+
+}
